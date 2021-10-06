fedb9a81e00d7f2d83500ced31b65467c7fbd001
David Post <davecpost@gmail.com>
2021-10-06 10:18:04 -0600

    first commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by David Post on 2021-10-06.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
