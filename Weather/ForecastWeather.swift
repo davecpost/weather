8799b66ba96ec8082ceede4e638c99e2507a8c8d
David Post <davecpost@gmail.com>
2021-10-06 10:39:59 -0600

    add ignore

---
+++ Weather/ForecastWeather.swift
@@ -0,0 +1,8 @@
+//
+//  ForecastWeather.swift
+//  Weather
+//
+//  Created by David Post on 2021-10-06.
+//
+
+import Foundation
---
+++ Weather/WeatherService.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherService.swift
+//  Weather
+//
+//  Created by David Post on 2021-10-06.
+//
+
+import Foundation
